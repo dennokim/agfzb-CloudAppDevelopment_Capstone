50794b1d0eb7d61ebf165683a8a94617616ac75b
Denis Mwangi <deniskiamni2022@gmail.com>
2024-02-09 07:51:19 -0500

    Add modified files: models.py, restapis.py, urls.py, views.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.utils.timezone import now
 
 
+# Create your models here.
+
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
 # - Name
 # - Description
@@ -50,21 +52,54 @@ class CarModel(models.Model):
 
     def __str__(self):
         return "Name: " + self.name
-
+        
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 class CarDealer:
+
     def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip):
+        # Dealer address
         self.address = address
+        # Dealer city
         self.city = city
+        # Dealer Full Name
         self.full_name = full_name
+        # Dealer id
         self.id = id
+        # Location lat
         self.lat = lat
+        # Location long
         self.long = long
+        # Dealer short name
         self.short_name = short_name
-        self.st = ste
+        # Dealer state
+        self.st = st
+        # Dealer zip
         self.zip = zip
 
     def __str__(self):
         return "Dealer name: " + self.full_name
 
+
 # <HINT> Create a plain Python class `DealerReview` to hold review data
+class DealerReview:
+
+    def __init__(self, dealership, name, purchase, review):
+        # Required attributes
+        self.dealership = dealership
+        self.name = name
+        self.purchase = purchase
+        self.review = review
+        # Optional attributes
+        self.purchase_date = ""
+        self.purchase_make = ""
+        self.purchase_model = ""
+        self.purchase_year = ""
+        self.sentiment = ""
+        self.id = ""
+
+    def __str__(self):
+        return "Review: " + self.review
+
+    def to_json(self):
+        return json.dumps(self, default=lambda o: o.__dict__,
+                            sort_keys=True, indent=4)
